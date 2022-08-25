27886103325d07c41cf38dca0b6875c54d6e8b0b
Jandueza <jonathanandueza1201@gmail.com>
2022-08-25 01:31:56 -0300

    Cosas por hacer en el .txt

--- js/main.js
+++ js/main.js
@@ -7,22 +7,21 @@ const idNumber = document.querySelector('#id-number');
 const address = document.querySelector('#address');
 const zipCode = document.querySelector('#zipcode');
 const phone = document.querySelector('#phone');
-const email = document.querySelector('#email');
+const username = document.querySelector('#username');
 const password = document.querySelector('#password');
+const errorDisplay = document.querySelector('.error-display');
+const resetButton = document.querySelector('.reset-button');
 
-// const DNIRadio = document.getElementById('DNI');
-// const CUILRadio = document.getElementById('CUIL');
 
-const regExpFirstName = new RegExp('^[a-zA-ZÁÉÍÓÚÑÜáéíóúñü][a-záéíóúñü]{2,9}$');
-const regExpLastName = new RegExp(`^[a-zA-ZÁÉÍÓÚÑÜáéíóúñü][A-ZÁÉÍÓÚÑÜa-záéíóúñü']{1,19}$`);
+const regExpFirstName = new RegExp('^([a-zA-ZÁÉÍÓÚÑÜáéíóúñü][a-záéíóúñü]{2,9})(\\s[a-zA-ZÁÉÍÓÚÑÜáéíóúñü][a-záéíóúñü]{2,9})?$');
+const regExpLastName = new RegExp(`^([a-zA-ZÁÉÍÓÚÑÜáéíóúñü][A-ZÁÉÍÓÚÑÜa-záéíóúñü']{1,19})(\\s[a-zA-ZÁÉÍÓÚÑÜáéíóúñü][A-ZÁÉÍÓÚÑÜa-záéíóúñü']{1,19}$)?$`);
 const regExpIdNumber = new RegExp("^([1-9]|[\\d][1-9])\\.[\\d]{3}\\.[\\d]{3}$|^((0[1-9][\\d]{6})|([1-9][\\d]{6,7}))$");
 const regExpCUIL = new RegExp("^(20|23|27|30|33)(([0-9][1-9][0-9]{6}|[1-9][0-9]{7})|(-([0-9][1-9][0-9]{6}|[1-9][0-9]{7}))-)[0-9]$");
 const regExpZipCode = new RegExp("^[0-9]{4}$");
 const regExpAddress = new RegExp(`^[A-Za-z\\dÁÉÍÓÚÑÜáéíóúñüÀÂÃÊÓÔÕàâãêôõÇ\\s\\.\\,\\-\\(\\)\\'\\"\\°\\/]{10,200}$`);
 const regExpPhone = new RegExp(`^(\\+54|0)?(9[1-9]|[1-9][1-9]|[1-9][1-9][1-9])([0-9]{4}|[0-9]{3})([0-9]{4})$`);
-const regExpEmail = new RegExp (`~^[a-zA-Z0-9.!#$%&'*+/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/`);
+const regExpUsername = new RegExp(`^[A-Z][a-z0-9]{6,20}$`);
 const regExpPassword = new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,16})`);
-
 
 // 2. Core functions:
 
@@ -46,7 +45,39 @@ function clearInput(e) {
     } return false
 }
 
+function displayErrorInLog (error) {
+    let divError = document.createElement('div');
+    let headerError = document.createElement('h3');
+    let msgError = document.createElement('p');
+    divError.classList.add('error-display__item');
+    headerError.classList.add('error-display__item__header');
+    msgError.classList.add('error-display__item__msg');
+    headerError.innerHTML = error.name;
+    msgError.innerHTML = error.message;
+    divError.appendChild(headerError);
+    divError.appendChild(msgError);
+    errorDisplay.appendChild(divError);
+    return divError
+}
 
+function createError (err, msg) {
+    let error = new Error(msg);
+    error.name = err;
+    return error
+}
+
+function trimValue (ev) {
+    ev.target.value = ev.target.value.trim();
+}
+
+function displayWarningError (ev, err) {
+    let divError = document.createElement('div');
+    divError.classList.add('error-display__popup');
+    divError.innerHTML = err.message;
+    ev.target.insertAdjacentElement('afterend', divError);
+    setTimeout(() => { divError.remove() }, 1500);
+    return divError
+}
 
 // 3. Adding Events
 
@@ -64,24 +95,33 @@ document.querySelectorAll('.input-group-idtype__input').forEach(inputName => {
 
 firstName.addEventListener('change', e =>{
     if(clearInput(e)){return}
+    trimValue(e);
     if(validation(e.target.value, regExpFirstName) || (e.target.value === '')){
         e.target.style.backgroundColor= 'green';
     } else {
         e.target.style.backgroundColor= 'red';
+        let error = createError('Error en el campo de nombre', 'Nombre inválido. Ingrese solo letras mayúsc. o minúsc.');
+        displayWarningError(e, error);
+        displayErrorInLog(error);
     }
 })
 
 lastName.addEventListener('change', e =>{
     if(clearInput(e)){return}
+    trimValue(e);
     if(validation(e.target.value, regExpLastName)){
         e.target.style.backgroundColor= 'green';
     } else {
         e.target.style.backgroundColor= 'red';
+        let error = createError ('Error en el campo de apellido', 'Apellido inválido. Ingrese solo letras mayúsc. o minúsc.');
+        displayWarningError(e, error);
+        displayErrorInLog(error);
     }
 })
 
 idNumber.addEventListener('change', e => {
     if(clearInput(e)){return}
+    trimValue(e);
     let validated;
     if(idType.value === 'DNI'){
         validated = validation(e.target.value, regExpIdNumber);
@@ -89,47 +129,66 @@ idNumber.addEventListener('change', e => {
     } else {
         validated = validation(e.target.value, regExpCUIL);
     }
-
+    
     if(validated){
         e.target.style.backgroundColor= 'green';
     } else {
         e.target.style.backgroundColor= 'red';
+        let error = createError (`Error en el campo de número de ${idType.value}`, `Número de ${idType.value} inválido. Ingrese un número de ${idType.value} válido.`);
+        displayWarningError(e, error);
+        displayErrorInLog(error);
     }
 })
 
 address.addEventListener('change', e =>{
     if(clearInput(e)){return}
+    trimValue(e);
     if(validation(e.target.value, regExpAddress) || (e.target.value === '')){
         e.target.style.backgroundColor= 'green';
     } else {
         e.target.style.backgroundColor= 'red';
+        let error = createError ('Error en el campo de dirección', 'Dirección inválida. Ingrese una dirección válida.');
+        displayWarningError(e, error);
+        displayErrorInLog(error);
     }
 })
 
 zipCode.addEventListener('change', e =>{
     if(clearInput(e)){return}
+    trimValue(e);
     if(validation(e.target.value, regExpZipCode || (e.target.value === ''))){
         e.target.style.backgroundColor= 'green';
     } else {
         e.target.style.backgroundColor= 'red';
+        let error = createError ('Error en el campo de código postal', 'Código postal inválido. Ingrese un código postal válido.');
+        displayWarningError(e, error);
+        displayErrorInLog(error);
     }
 })
 
 phone.addEventListener('change', e =>{
     if(clearInput(e)){return}
+    trimValue(e);
     if(validation(e.target.value, regExpPhone) || (e.target.value === '')){
         e.target.style.backgroundColor= 'green';
     } else {
         e.target.style.backgroundColor= 'red';
+        let error = createError ('Error en el campo de teléfono', 'Teléfono inválido. Ingrese un teléfono válido.');
+        displayWarningError(e, error);
+        displayErrorInLog(error);
     }
 })
 
-email.addEventListener('change', e =>{
+username.addEventListener('change', e =>{
     if(clearInput(e)){return}
-    if(validation(e.target.value, regExpEmail) || (e.target.value === '')){
+    trimValue(e);
+    if(validation(e.target.value, regExpUsername) || (e.target.value === '')){
         e.target.style.backgroundColor= 'green';
     } else {
         e.target.style.backgroundColor= 'red';
+        let error = createError ('Error en el campo de nombre de usuario', 'Nombre de usuario inválido. Ingrese un nombre de usuario válido.');
+        displayWarningError(e, error);
+        displayErrorInLog(error);
     }
 })
 
@@ -139,5 +198,16 @@ password.addEventListener('change', e =>{
         e.target.style.backgroundColor= 'green';
     } else {
         e.target.style.backgroundColor= 'red';
+        let error = createError ('Error en el campo de contraseña', 'Contraseña inválida. Ingrese una contraseña válida.');
+        displayWarningError(e, error);
+        displayErrorInLog(error);
     }
+})
+
+resetButton.addEventListener('click', e => {
+    errorDisplay.innerHTML = '';
+    document.querySelectorAll('input').forEach(inputName => {
+        inputName.style.backgroundColor = '#ffffff';
+        idNumber.disabled = true;
+    } )
 })
